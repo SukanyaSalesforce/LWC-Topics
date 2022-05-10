4f6e40c17249d566496c97ebbf04aaa27761115b
sukku <sukanyam425@gmail.com>
2022-05-10 23:11:22 +0530

    added loopping cmp

---
+++ force-app/main/default/lwc/looping/looping.js
@@ -0,0 +1,26 @@
+import { LightningElement } from 'lwc';
+
+export default class Looping extends LightningElement {
+    emps=["sai","sukku","rani","baby"]
+    ceoList=[{
+        id:1,
+        company:'Apple',
+        name:'Tim Cook'
+    },
+    {
+        id:2,
+        company:'Google',
+        name:'Sundar Pichai'
+    },
+    {
+        id:3,
+        company:'Microsoft',
+        name:'Satya Nadella',
+    },
+    {
+        id:4,
+        company:'Amazon',
+        name:'Andy Jassy'
+    }
+]
+}
